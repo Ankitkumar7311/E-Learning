d6f3767ca4c9c0c74d6eeb9b832b000d56880ef4
zaib <zaib@gmail>
2025-08-24 13:12:06 +0530

    first half of page2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import academic from "../assets/academic.png"
+
 
 const Login = () => {
   return (
-    <div className="p-10 text-center">
+    <>
+    {/* <div className="p-10 text-center">
       <h1 className="text-3xl font-bold mb-4">Login Page</h1>
       <p className="text-gray-600">Here your login.</p>
+    </div> */}
+
+    <div className="relative w-full min-h-screen bg-cover "
+          style={{ backgroundImage: `url(${academic})` }}>
+
+        <div className="absolute top-30 left-30 text-3xl text-[white] h-[400px] w-[600px] leading-relaxed">
+
+          <h1 className="text-[70px]">VIDYA SARTHI</h1>
+
+          <h3 className="text-[50px]">Welcome to Vidya Sarthi</h3> <br />
+          <p>A digital bridge between students and teachers, where teachers share knowledge and students access notes with ease.</p>
+        </div>
+      
     </div>
+    
+
+
+    
+    </>
   );
 };
 
