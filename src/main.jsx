1675845f91cf0a7a8c6b02d57764142deb89c28f
zaib <zaib@gmail>
2025-09-01 10:36:45 +0530

    nothing to commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-// import "bootstrap/dist/css/bootstrap.min.css";
-=======
->>>>>>> 577c1d5ea4954502c13d2b5e8c44ad05ff53c0e8
+
 import "./index.css";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
