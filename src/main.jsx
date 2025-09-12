5c75791580bdde36fb26fdf0cd2f48d576ea9230
shifa <shifaxcam@gmail.com>
2025-09-12 11:29:37 +0530

    routing c3

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import "./index.css";
 import routes from "./routing/Router";
-import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <RouterProvider router={routes} />
-  </React.StrictMode>
+
 );
